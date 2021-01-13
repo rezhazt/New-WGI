301533091457479ea87efd05ce6ec4e7318e651c
Tito Harditya W <tito.w@students.amikom.ac.id>
2021-01-13 16:30:22 +0700

    Update print.php

--- print.php
+++ print.php
@@ -1,11 +1,52 @@
-<?php
-    include 'connection.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
-    $query = mysqli_query($kon, "SELECT * FROM tabel_visitor ORDER BY id DESC LIMIT 1");
+    <link rel="stylesheet" href="css/style.css">
+    <title>Print Hasil</title>
+</head>
+<body>
 
-    if(mysqli_num_rows($query)>0){
-        while($data = mysqli_fetch_array($query)){
-            echo $data["nama"];
-        }
-    }
-?>
+<!-- form login -->
+<section class="login_admin" id="login_admin">
+        <div class="container" style="margin-top: 50px;">
+            <div class="row">
+                <div class="col m8 12 offset-m2">
+                        <div class="card-panel">
+                        <?php
+                            include "connection.php";
+                            $sql="select * from tabel_visitor order by id desc limit 1";
+                            
+                            $hasil=mysqli_query($kon,$sql);
+                            $no=0;
+                            while ($data = mysqli_fetch_array($hasil)) {
+                            $no++;
+                            
+                                echo $no; 
+                                echo $data["no_id"]; 
+                                echo $data["name"]; 
+                                echo $data["phone"];
+                                echo $data["email"];
+                                echo $data["visit_date"];
+                                echo $data["tujuan"];
+                                echo $data["covid_form"];
+                            }
+                        ?>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end form login -->
+
+ <!--JavaScript at end of body for optimized loading-->
+ <script type="text/javascript" src="js/materialize.min.js"></script>
+
+</body>
+</html>
