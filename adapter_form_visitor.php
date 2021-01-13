617693752ad301111f66fb91ea047edccc9e64a2
Tito Harditya W <tito.w@students.amikom.ac.id>
2021-01-13 15:26:15 +0700

    Create adapter_form_visitor.php

---
+++ adapter_form_visitor.php
@@ -0,0 +1,23 @@
+<?php
+    include 'connection.php';
+
+    $no_id = $_POST["no_id"];
+    $name = $_POST["name"];
+    $phone=$_POST["phone"];
+    $email=$_POST["email"];
+    $visit_date=$_POST["visit_date"];
+    $tujuan=$_POST["tujuan"];
+    $covid_form=$_POST['covid_form'];
+
+    $sql = "insert into tabel_visitor (no_id,name,phone,email,visit_date,tujuan,covid_form) values ('$no_id','$name','$phone','$email','$visit_date','$tujuan','$covid_form')";
+
+    $hasil = mysqli_query($kon,$sql);
+
+    if($hasil){
+        echo "Berhasil";
+        exit;
+    } else {
+        echo "Gagal koneksi";
+        exit;
+    }
+?>
